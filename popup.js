f92351229e72ee3bf7748d42a7cf7065493cd934
Jorge Enrique Gamboa Fuentes <snake2209@hotmail.com>
2024-10-30 23:46:45 -0400

    NEW EXTENSION - FUNCTIONALITY

---
+++ popup.js
@@ -0,0 +1,22 @@
+// Función para enviar "Hello World" al textarea en la página
+function injectHelloWorld() {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        function: () => {
+          const textarea = document.getElementById("editor");
+          if (textarea) {
+            textarea.value = "Hello World!";
+          } else {
+            console.log("El elemento <textarea> no se encontró en la página.");
+          }
+        }
+      });
+    });
+  }
+  
+  // Escuchar el evento de clic en el botón y ejecutar la función
+  document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("sendButton").addEventListener("click", injectHelloWorld);
+  });
+  
