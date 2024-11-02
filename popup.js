bc34886fef0038d0096fead4b0d8e6e13bc23533
PacoSalazar <pa_ko_7@hotmail.com>
2024-11-02 10:49:51 -0600

    CFNS | My first extension

--- popup.js
+++ popup.js
@@ -1,22 +1,21 @@
 // Función para enviar "Hello World" al textarea en la página
 function injectHelloWorld() {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        function: () => {
-          const textarea = document.getElementById("editor");
-          if (textarea) {
-            textarea.value = "Hello World!, ENRIQUE WAS HERE, WITH Francisco, Javier and GUS 🪱";
-          } else {
-            console.log("El elemento <textarea> no se encontró en la página.");
-          }
-        }
-      });
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            function: () => {
+                const textarea = document.getElementById("editor");
+                if (textarea) {
+                    textarea.value = "Hello World!";
+                } else {
+                    console.log("El elemento <textarea> no se encontró en la página.");
+                }
+            }
+        });
     });
-  }
-  
-  // Escuchar el evento de clic en el botón y ejecutar la función
-  document.addEventListener("DOMContentLoaded", () => {
+}
+
+// Escuchar el evento de clic en el botón y ejecutar la función
+document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("sendButton").addEventListener("click", injectHelloWorld);
-  });
-  
+});
